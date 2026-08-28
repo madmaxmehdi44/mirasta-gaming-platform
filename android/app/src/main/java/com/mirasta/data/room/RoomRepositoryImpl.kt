00bd1dd0e96dd03753afd2e0c7f76694b79abda3
Medi <madmaxmehdi44@gmail.com>
2026-08-28 17:03:54 +0330

    Connect RoomRepositoryImpl to Nakama data source

--- android/app/src/main/java/com/mirasta/data/room/RoomRepositoryImpl.kt
+++ android/app/src/main/java/com/mirasta/data/room/RoomRepositoryImpl.kt
@@ -3,25 +3,27 @@ package com.mirasta.data.room
 import com.mirasta.core.model.Room
 import com.mirasta.domain.room.RoomRepository
 
-class RoomRepositoryImpl : RoomRepository {
+class RoomRepositoryImpl(
+    private val dataSource: NakamaRoomDataSource
+) : RoomRepository {
 
     private var currentRoom: Room? = null
 
     override suspend fun createRoom(name: String): Result<Room> {
-        val room = Room(
-            id = "local-room",
-            name = name,
-            memberCount = 1
-        )
-        currentRoom = room
-        return Result.success(room)
+        val result = dataSource.create(name)
+        if (result.isSuccess) {
+            currentRoom = result.getOrNull()
+        }
+        return result
     }
 
     override suspend fun joinRoom(roomId: String): Result<Unit> {
+        dataSource.join(roomId)
         return Result.success(Unit)
     }
 
     override suspend fun leaveRoom() {
+        dataSource.leave()
         currentRoom = null
     }
 }
